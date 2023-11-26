de318fb6d3eb44061a178571dcf0e9551923bd41
AlexyJoaquim-Reino <98829232+AlexyJoaquim-Reino@users.noreply.github.com>
2023-11-26 18:53:29 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -18,3 +18,77 @@ $style.html(style);
 }).on("mouseout", function() {
     $cards.removeClass("active");
 });
+
+
+// Exemple de structure JSON avec les informations sur les images des cartes
+const cartesJSON = {
+    "cartes": [
+        {
+            "nom": "card1",
+            "url": "https://imgur.com/xiJF6at.png"
+          },
+          {
+            "nom": "card2",
+            "url": "https://imgur.com/7KPi4Vh.png"
+          },
+          {
+            "nom": "card3",
+            "url": "https://imgur.com/EJo1ZeT.png"
+          },
+          {
+            "nom": "card4",
+            "url": "https://imgur.com/Z76LqFi.png"
+          },
+          {
+            "nom": "card5",
+            "url": "https://imgur.com/nPetBtP.png"
+          },
+          {
+            "nom": "card6",
+            "url": "https://imgur.com/wf4e6zO.png"
+          },
+          {
+            "nom": "card7",
+            "url": "https://imgur.com/4DeHhgZ.jpg"
+          },
+          {
+            "nom": "card8",
+            "url": "https://imgur.com/Zi7eOJf.png"
+          },
+    ]
+  };
+
+ 
+  
+  function chargerCartes() {
+    const conteneur = document.getElementById('card-container');
+  
+    // Récupère les informations sur les cartes du JSON
+    const cartes = cartesJSON.cartes;
+  
+    // Boucle pour créer les éléments HTML pour chaque carte
+    for (let i = 0; i < cartes.length; i++) {
+      const carte = cartes[i];
+  
+      // Crée un élément div pour la carte
+      const cardDiv = document.createElement('div');
+      cardDiv.classList.add('card');
+      cardDiv.onclick = function () { agrandirCarte(this); };
+  
+      // Crée un élément img avec l'URL de l'image
+      const imgElement = document.createElement('img');
+      imgElement.src = carte.url;
+      imgElement.alt = carte.nom;
+  
+      // Ajoute l'élément img à l'élément div
+      cardDiv.appendChild(imgElement);
+  
+      // Ajoute l'élément div au conteneur
+      conteneur.appendChild(cardDiv);
+    }
+  }
+  
+  function agrandirCarte(carte) {
+    carte.classList.toggle('agrandie');
+  }
+  
